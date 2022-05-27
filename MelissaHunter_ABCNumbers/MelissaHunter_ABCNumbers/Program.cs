cb5b930249a1fb4833db7017994bcaa4a67c7c22
Melissa H <60014977+HunterM22@users.noreply.github.com>
2022-05-26 20:54:14 -0400

    Add files via upload

---
+++ MelissaHunter_ABCNumbers/MelissaHunter_ABCNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace OP_MelissaHunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ABCNumberProblem();
+            //Main calls the method
+        }
+
+        private static void ABCNumberProblem()
+        {
+            string output = string.Empty; //Initialize empty string
+            for (int i = 1; i <= 5; i++) //Loop to increment the integer
+            {
+                int number = i;
+                string letters = "abc";
+                output += string.Concat(letters, (i.ToString())); //Convert int to string and concatenate results
+            }
+            Console.WriteLine(output); //Print the output result to the screen
+        }
+
+
+
+
+    }
+}
